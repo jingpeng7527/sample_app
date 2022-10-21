bcf0b8700008678385323c1f04d09ea9e3425e85
Zoeyzyzyzy <zeyuwang@ucsb.edu>
2022-10-20 17:48:14 -0700

    finish all requirements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "hello, world!"
-  end
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+ 
+  private
+    def record_not_found
+      render plain: "Opps...404 Not Found", status: 404
+    end
 end
